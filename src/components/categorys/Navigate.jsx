887de75504a3aefded191f0cfd5070d2aa72d42b
Bryan <bryansanudo95@gmail.com>
2023-04-05 14:34:33 -0500

    bug deploy

--- src/components/categorys/Navigate.jsx
+++ src/components/categorys/Navigate.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
-
-import logoGelam from "@/assets/logoGelam.jpg";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navigate = () => {
   const useScroll = () => {
